02deeedd4ec0b656bb025fe1015cb83985187700
Staś Małolepszy <stas@piesku.com>
2019-10-03 12:00:16 +0200

    Move the camera in XZ plane

--- src/blueprints/blu_camera.ts
+++ src/blueprints/blu_camera.ts
@@ -7,11 +7,11 @@ import {Blueprint} from "./blu_common";
 
 export function create_camera(game: Game) {
     return <Blueprint>{
-        Rotation: from_euler([], 45, -135, 0),
+        Rotation: from_euler([], 0, -135, 0),
         Using: [player_control(true), move(10, 0.2)],
         Children: [
             {
-                Rotation: [0, 1, 0, 0],
+                Rotation: from_euler([], -45, 180, 0),
                 Using: [camera(game.Canvas.width / game.Canvas.height, 1, 0.1, 1000)],
             },
         ],
